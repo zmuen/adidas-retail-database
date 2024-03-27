ac726835712c23dba7a67963371eebfba7a0f3cb
Muen Zhang <75929368+zmuen@users.noreply.github.com>
2024-03-27 17:48:56 -0400

    Add files via upload

---
+++ scripts/CREATE TABLE colour.sql
@@ -0,0 +1,17 @@
+CREATE TABLE colour (
+    colour_name VARCHAR(255),
+    hex VARCHAR(50) PRIMARY KEY
+);
+
+INSERT INTO colour (colour_name, hex) VALUES
+('Blue', '#000000'),
+('Blue', '#0000FF'),
+('Gray', '#808080'),
+('Green', '##008000'),
+('Orange', '#FF7F00'),
+('Pink', '#FFC0CB'),
+('Purple', '#9678B6'),
+('Red', '#FF0000'),
+('Silver', '#C0C0C0'),
+('White', '#FFFFFF'),
+('Yellow', '#FFFF00');
